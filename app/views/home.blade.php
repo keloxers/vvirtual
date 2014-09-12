d83fa1d72a802c25de0e85dfeb3624ebd1ed4718
Miguel Mendez <keloxers@gmail.com>
2014-09-12 07:39:23 -0300

    uploads

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -125,6 +125,106 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Title bar -->
+<div class="pi-section-w pi-section-base pi-section-base-gradient">
+	<div class="pi-texture" style="background: url(img/hexagon.png) repeat;"></div>
+	<div class="pi-section" style="padding: 15px 40px 12px;">
+
+		<div class="pi-row">
+			<div class="pi-col-sm-8 pi-center-text-xs">
+				<h1 class="h2 pi-weight-300 pi-margin-bottom-5">Más vistas de la semana</h1>
+			</div>
+		</div>
+
+	</div>
+</div>
+<!-- End title bar -->
+
+
+<div id="page"><!-- - - - - - - - - - SECTION - - - - - - - - - -->
+
+	<div class="pi-section-w pi-section-white">
+		<div class="pi-section">
+
+			<!-- Row -->
+			<div class="pi-row">
+
+
+				@foreach ($articulos_masvistos as $articulo)
+
+				<?php
+					$texto = $articulo->texto;
+					$archivos = DB::table('archivos')
+					->where('padre', '=', 'articulo')
+					->where('padre_id', '=', $articulo->id)
+					->first();
+
+					if (preg_match('/^.{1,110}\b/s', $articulo->texto, $match)){
+						$texto = $match[0];
+					}
+					$categoria = Categoria::find($articulo->categorias_id);
+					?>
+
+
+
+
+					<!-- Col 4 -->
+					<div class="pi-col-xs-3">
+
+						<!-- Post item -->
+						<div class="pi-img-w pi-img-round-corners pi-img-shadow">
+							<a href="/articulos/show/{{ $articulo->url_seo }}">
+								<?php if (count($archivos)>0 )  { ?>
+									<img src="/uploads/crop/{{ $archivos->archivo }}" alt="">
+									<? } ?>
+									<span class="pi-img-overlay pi-img-overlay-white">
+									</span>
+								</a>
+							</div>
+
+							<h2 class="h5 pi-margin-bottom-5">
+								<a href="/articulos/show/{{ $articulo->url_seo }}" class="pi-link-dark">{{ $articulo->articulo }}</a>
+							</h2>
+							<ul class="pi-meta pi-margin-bottom-10">
+								<li><i class="icon-clock"></i>{{ $articulo->created_at }}</li>
+								<li><i class="icon-eye"></i>{{ $articulo->visitas }} visitas</li>
+							</ul>
+							<p>
+								{{ $texto }}... <a href="/articulos/show/{{ $articulo->url_seo }}" class="pi-italic">Leer</a>
+							</p>
+							<!-- End post item -->
+
+						</div>
+						<!-- End col 4 -->
+
+
+						@endforeach
+					</div>
+					<!-- End row -->
+				</div>
+			</div>
+		</div>
+		
+
+
+
+
+
+
+
 						</div>
 					</div>
 				</div>
