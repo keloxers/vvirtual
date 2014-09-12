e49e0099127503c749bc2ea0afdd5122572e4477
Miguel Mendez <keloxers@gmail.com>
2014-09-12 07:00:54 -0300

    uploads
    noticias

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -2,4 +2,22 @@
 @section('content')
 
 
+<!-- Title bar -->
+<div class="pi-section-w pi-section-base pi-section-base-gradient">
+	<div class="pi-texture" style="background: url(img/hexagon.png) repeat;"></div>
+		<div class="pi-section" style="padding: 15px 40px 12px;">
+
+			<div class="pi-row">
+				<div class="pi-col-sm-8 pi-center-text-xs">
+					<h1 class="h2 pi-weight-300 pi-margin-bottom-5">Noticias de tapa</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+<!-- End title bar -->
+
+
+
+
+
 @stop
