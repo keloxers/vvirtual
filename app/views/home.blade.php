2e217ed347ff0d41a40b443d703595196efcab8c
Miguel Mendez <keloxers@gmail.com>
2014-09-12 18:41:46 -0300

    mas visitadas

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -4,7 +4,7 @@
 
 
 <?php if (count($articulos_tapa)) { ?>
-<?php } ?>
+	<?php } ?>
 
 
 
@@ -19,12 +19,12 @@
 			@if ($imagen[0]<>"")
 			<!-- Title bar -->
 
-					<div class="pi-row">
-						<div class="pi-section-w pi-center-text-xs pi-text-center">
+			<div class="pi-row">
+				<div class="pi-section-w pi-center-text-xs pi-text-center">
 
-							<a href="{{$imagen[1]}}"><img src="{{$imagen[0]}}" alt=""></a>
-						</div>
-					</div>
+					<a href="{{$imagen[1]}}"><img src="{{$imagen[0]}}" alt=""></a>
+				</div>
+			</div>
 
 			<!-- End title bar -->
 			@endif
@@ -34,9 +34,9 @@
 
 
 
-<!-- Title bar -->
-<div class="pi-section-w pi-section-base pi-section-base-gradient">
-	<div class="pi-texture" style="background: url(img/hexagon.png) repeat;"></div>
+	<!-- Title bar -->
+	<div class="pi-section-w pi-section-base pi-section-base-gradient">
+		<div class="pi-texture" style="background: url(img/hexagon.png) repeat;"></div>
 		<div class="pi-section" style="padding: 15px 40px 12px;">
 
 			<div class="pi-row">
@@ -46,7 +46,7 @@
 			</div>
 		</div>
 	</div>
-<!-- End title bar -->
+	<!-- End title bar -->
 
 
 
@@ -184,11 +184,93 @@
 
 
 
+		<div id="page"><!-- - - - - - - - - - SECTION - - - - - - - - - -->
+
+			<div class="pi-section-w pi-section-white">
+				<div class="pi-section">
+
+					<!-- Row -->
+					<div class="pi-row">
 
 
 
+						@foreach ($articulos_masvistos as $articulo)
+						<?php
+						$texto = $articulo->texto;
+						$archivos = DB::table('archivos')
+						->where('padre', '=', 'articulo')
+						->where('padre_id', '=', $articulo->id)
+						->first();
+
+						if (preg_match('/^.{1,110}\b/s', $articulo->texto, $match))
+						{ $texto = $match[0]; }
+						$categoria = Categoria::find($articulo->categorias_id);
+						?>
+
+
+
+
+						<!-- Col 4 -->
+						<div class="pi-col-xs-3">
+
+							<!-- Post item -->
+							<div class="pi-img-w pi-img-round-corners pi-img-shadow">
+								<a href="/articulos/show/{{ $articulo->url_seo }}">
+									@if (count($archivos)>0 )
+										<img src="/uploads/crop/{{ $archivos->archivo }}" alt="">
+									@endif
+									<span class="pi-img-overlay pi-img-overlay-white">
+									</span>
+									</a>
+								</div>
+
+								<h2 class="h5 pi-margin-bottom-5">
+									<a href="/articulos/show/{{ $articulo->url_seo }}" class="pi-link-dark">{{ $articulo->articulo }}</a>
+								</h2>
+								<ul class="pi-meta pi-margin-bottom-10">
+									<li><i class="icon-clock"></i>{{ $articulo->created_at }}</li>
+									<li><i class="icon-eye"></i>{{ $articulo->visitas }} visitas</li>
+								</ul>
+								<p>
+									{{ $texto }}... <a href="/articulos/show/{{ $articulo->url_seo }}" class="pi-italic">Leer</a>
+								</p>
+								<!-- End post item -->
+
+							</div>
+							<!-- End col 4 -->
+
+
+							@endforeach
+						</div>
+						<!-- End row -->
+					</div>
+				</div>
+			</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		</div>
 	</div>
-</div>
 </div>
 
 @stop
