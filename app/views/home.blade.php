6b257776bb20d95794ccf462412f1c55cd67abb5
Miguel Mendez <keloxers@gmail.com>
2014-09-12 07:23:55 -0300

    jaja

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -49,30 +49,7 @@
 							?>
 
 
-							<div class="Photography pi-col-md-4 pi-col-sm-4 pi-col-xs-6 isotope-item">
-								<div class="pi-img-w pi-img-hover-zoom">
-									<?php if (count($archivos)>0 )  { ?>
-										<img src="/uploads/crop/{{ $archivos->archivo }}" alt="">
-										<?php } ?>
-										<div class="pi-img-overlay pi-img-overlay-darker">
-											<div class="pi-caption-centered">
-												<div>
-													<?php if (count($archivos)>0 )  { ?>
-														<a href="/uploads/big/{{ $archivos->archivo }}" class="pi-colorbox">
-															<?php } ?>
-															<span class="pi-caption-icon pi-caption-scale icon-search"></span>
-														</a>
-														<h3 class="h4 pi-weight-300"><a href="/articulos/show/{{ $articulo->url_seo }}" class="pi-link-white">{{ $articulo->articulo }}</a></h3>
-														<ul class="pi-caption-links">
-															<li><i class="icon-tag"></i>{{ $categoria->categoria }}</li>
-														</ul>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
+
 						</div>
 						<!-- - - - - - - - - - END SECTION - - - - - - - - - -->
 					</div>
