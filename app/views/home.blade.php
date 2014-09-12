6a643f5c7875e5bc671966611b68ff51e9d58b71
Miguel Mendez <keloxers@gmail.com>
2014-09-12 18:28:28 -0300

    principal

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -3,27 +3,13 @@
 
 
 
-	<!-- Title bar -->
-	<div class="pi-section-w pi-section-base pi-section-base-gradient">
-		<div class="pi-texture" style="background: url(img/hexagon.png) repeat;"></div>
-		<div class="pi-section" style="padding: 15px 40px 12px;">
-
-			<div class="pi-row">
-				<div class="pi-col-sm-8 pi-center-text-xs">
-					<h1 class="h2 pi-weight-300 pi-margin-bottom-5">Noticias de tapa</h1>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- End title bar -->
-
 <?php if (count($articulos_tapa)) { ?>
 <?php } ?>
 
 
 
 	<div id="page"><!-- - - - - - - - - - SECTION - - - - - - - - - -->
-		<div class="pi-section-w pi-section-white piCaptions">
+		<div class="pi-section-w pi-section-white">
 
 			<?php
 			$banner = new Banner();
