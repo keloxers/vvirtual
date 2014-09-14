d1468e6e992ff57b9e22033b9f7664529c220c8b
Miguel Mendez <keloxers@gmail.com>
2014-09-14 10:04:53 -0300

    home page pagandom

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -139,12 +139,14 @@
 
 
 			</div>
-			<div class="pi-pagenav pi-text-center">
-				{{ $articulos->links()}}
-			</div>
 
 		</div>
 
+		<div class="pi-pagenav pi-text-center">
+			{{ $articulos->links()}}
+		</div>
+
+	</div>
 
 
 
@@ -160,9 +162,7 @@
 
 
 
-
-
-
+	<div id="page"><!-- - - - - - - - - - SECTION - - - - - - - - - -->
 
 
 
@@ -220,60 +220,60 @@
 							<div class="pi-img-w pi-img-round-corners pi-img-shadow">
 								<a href="/articulos/show/{{ $articulo->url_seo }}">
 									@if (count($archivos)>0 )
-										<img src="/uploads/crop/{{ $archivos->archivo }}" alt="">
+									<img src="/uploads/crop/{{ $archivos->archivo }}" alt="">
 									@endif
 									<span class="pi-img-overlay pi-img-overlay-white">
 									</span>
-									</a>
-								</div>
-
-								<h2 class="h5 pi-margin-bottom-5">
-									<a href="/articulos/show/{{ $articulo->url_seo }}" class="pi-link-dark">{{ $articulo->articulo }}</a>
-								</h2>
-								<ul class="pi-meta pi-margin-bottom-10">
-									<li><i class="icon-clock"></i>{{ $articulo->created_at }}</li>
-									<li><i class="icon-eye"></i>{{ $articulo->visitas }} visitas</li>
-								</ul>
-								<p>
-									{{ $texto }}... <a href="/articulos/show/{{ $articulo->url_seo }}" class="pi-italic">Leer</a>
-								</p>
-								<!-- End post item -->
-
+								</a>
 							</div>
-							<!-- End col 4 -->
 
+							<h2 class="h5 pi-margin-bottom-5">
+								<a href="/articulos/show/{{ $articulo->url_seo }}" class="pi-link-dark">{{ $articulo->articulo }}</a>
+							</h2>
+							<ul class="pi-meta pi-margin-bottom-10">
+								<li><i class="icon-clock"></i>{{ $articulo->created_at }}</li>
+								<li><i class="icon-eye"></i>{{ $articulo->visitas }} visitas</li>
+							</ul>
+							<p>
+								{{ $texto }}... <a href="/articulos/show/{{ $articulo->url_seo }}" class="pi-italic">Leer</a>
+							</p>
+							<!-- End post item -->
 
-							@endforeach
 						</div>
-						<!-- End row -->
+						<!-- End col 4 -->
+
+
+						@endforeach
 					</div>
+					<!-- End row -->
 				</div>
 			</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 		</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	</div>
+</div>
 </div>
 
 @stop
