dc9ec3fd9f04b183b9270946e42ef4b16284d39c
Miguel Mendez <keloxers@gmail.com>
2014-11-10 20:24:48 -0300

    turnos

--- app/views/turnos/index.blade.php
+++ app/views/turnos/index.blade.php
@@ -10,6 +10,7 @@
 		<div class="pi-section-w pi-section-white">
 			<div class="pi-section">
 
+				<a href='/turnos/create' class='btn pi-btn-base pi-btn-small'>Nuevo turno</a><br>
 
 				<hr class="pi-divider-gap-70">
 
