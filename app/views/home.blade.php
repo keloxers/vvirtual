f2fd552978ae4342f33230db42e4713a57374a66
Miguel Mendez <keloxers@gmail.com>
2015-11-22 18:50:46 -0300

    go

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -22,7 +22,7 @@
 			<!-- Counters item -->
 			<div class="pi-col-lg-3">
 				<div class="pi-box pi-border pi-shadow2">
-					<div class="pi-counter pi-counter-simple" data-count-from="0" data-count-to="0" data-easing="easeInCirc" data-duration="2000" data-frames-per-second="10">
+					<div class="pi-counter pi-counter-simple" data-count-from="0" data-count-to="473" data-easing="easeInCirc" data-duration="2000" data-frames-per-second="10">
 						<div class="pi-counter-count pi-counter-count-big">
 
 							<p>
@@ -42,7 +42,7 @@
 			<!-- Counters item -->
 			<div class="pi-col-lg-3">
 				<div class="pi-box pi-border pi-shadow2">
-					<div class="pi-counter pi-counter-simple" data-count-from="0" data-count-to="0" data-easing="easeInCirc" data-duration="3000" data-frames-per-second="10">
+					<div class="pi-counter pi-counter-simple" data-count-from="0" data-count-to="343" data-easing="easeInCirc" data-duration="3000" data-frames-per-second="10">
 						<div class="pi-counter-count pi-counter-count-big">
 
 							<p>
@@ -66,6 +66,13 @@
 		</div>
 		<!-- End counters -->
 
+		<div class="pi-text-center">
+			<h2 class="pi-uppercase pi-weight-700  pi-has-tall-border pi-has-short-border pi-margin-bottom-70">
+				Mesas escrutadas<br> 3 de 71
+			</h2>
+		</div>
+
+
 		<hr class="pi-divider-gap-30">
 
 		<p>
@@ -80,6 +87,34 @@
 	</div>
 </div>
 <!-- - - - - - - - - - END SECTION - - - - - - - - - --></div>
+
+<script src="/3dParty/jquery-1.11.0.min.js"></script>
+<script src="/scripts/pi.easings.js"></script>
+<script src="/scripts/pi.helpers.js"></script>
+<script src="/scripts/pi.boundManager.js"></script>
+<script src="/3dParty/inview.js"></script>
+<script src="/scripts/pi.imagesLoader.js"></script>
+<script src="/3dParty/gauge.min.js"></script>
+<script src="/scripts/pi.counter.js"></script>
+<script src="/scripts/pi.init.counter.js"></script>
+<script src="/scripts/pi.init.social.js"></script>
+<script src="/scripts/pi.ddMenu.js"></script>
+<script src="/scripts/pi.init.removeLastElMargin.js"></script>
+<script src="/scripts/pi.fixedHeader.js"></script>
+<script src="/scripts/pi.mobileMenu.js"></script>
+<script src="/scripts/pi.init.submitFormFooter.js"></script>
+<script src="/scripts/pi.detectTransition.js"></script>
+<script src="/scripts/pi.alert.js"></script>
+<script src="/scripts/pi.init.formsBlurClasses.js"></script>
+<script src="/scripts/pi.init.placeholder.js"></script>
+<script src="/3dParty/colorbox/jquery.colorbox-min.js"></script>
+<script src="/scripts/pi.init.colorbox.js"></script>
+<script src="/3dParty/jquery.easing.1.3.js"></script>
+<script src="/3dParty/jquery.scrollTo.min.js"></script>
+<script src="/scripts/pi.init.jqueryScrollTo.js"></script>
+<script src="/scripts/pi.scrollTopArrow.js"></script>
+<script src="/3dParty/tweetie/tweetie.min.js"></script>
+<script src="/scripts/pi.init.tweetie.js"></script>
 
 
 @stop
