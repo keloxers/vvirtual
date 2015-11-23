26ba0f730260228529cadd401f38630e31aeb9b5
Miguel Mendez <keloxers@gmail.com>
2015-11-23 09:15:40 -0300

    home

---
+++ app/views/homeelecciones.blade.php
@@ -0,0 +1,120 @@
+@extends('layouts.default')
+@section('content')
+
+<div id="page"><!-- - - - - - - - - - SECTION - - - - - - - - - -->
+<div class="pi-section-w pi-section-white">
+	<div class="pi-section pi-padding-bottom-80">
+
+		<div class="pi-text-center">
+			<h1 class="pi-uppercase pi-weight-700 pi-has-border pi-has-tall-border pi-has-short-border pi-margin-bottom-70">
+				Resultados FINALES en<br>Virasoro Corrientes
+			</h1>
+		</div>
+
+		<!-- Counters -->
+		<div class="pi-row pi-text-center">
+
+			<!-- Counters item -->
+			<div class="pi-col-lg-3">
+			</div>
+			<!-- End counters item -->
+
+			<!-- Counters item -->
+			<div class="pi-col-lg-3">
+				<div class="pi-box pi-border pi-shadow2">
+					<div class="pi-counter pi-counter-simple" data-count-from="0" data-count-to="11162" data-easing="easeInCirc" data-duration="2000" data-frames-per-second="10">
+						<div class="pi-counter-count pi-counter-count-big">
+
+							<p>
+								<img src="/img/scioli.jpg" alt="">
+
+							</p>
+
+							<h2><span class="pi-counter-number">0</span></h2>
+
+						</div>
+						<p>Daniel Scioli</p>
+					</div>
+				</div>
+			</div>
+			<!-- End counters item -->
+
+			<!-- Counters item -->
+			<div class="pi-col-lg-3">
+				<div class="pi-box pi-border pi-shadow2">
+					<div class="pi-counter pi-counter-simple" data-count-from="0" data-count-to="8495" data-easing="easeInCirc" data-duration="3000" data-frames-per-second="10">
+						<div class="pi-counter-count pi-counter-count-big">
+
+							<p>
+								<img src="/img/macri.jpg" alt="">
+							</p>
+
+							<h2><span class="pi-counter-number">0</span></h2>
+
+						</div>
+						<p>Mauricio Macri</p>
+					</div>
+				</div>
+			</div>
+			<!-- End counters item -->
+
+			<!-- Counters item -->
+			<div class="pi-col-lg-3">
+			</div>
+			<!-- End counters item -->
+
+		</div>
+		<!-- End counters -->
+
+		<div class="pi-text-center">
+			<h2 class="pi-uppercase pi-weight-700  pi-has-tall-border pi-has-short-border pi-margin-bottom-70">
+				Mesas escrutadas<br> 71 de 71
+			</h2>
+		</div>
+
+
+		<hr class="pi-divider-gap-30">
+
+		<p>
+			Los resultados son parciales, recolectados de las planillas de los fiscales que trabajan en cada escuela y corresponden solamente a la localidad de Gdor. Virasoro
+		</p>
+
+		<hr class="pi-divider-gap-10">
+
+
+		<!-- End counters -->
+
+	</div>
+</div>
+<!-- - - - - - - - - - END SECTION - - - - - - - - - --></div>
+
+<script src="/3dParty/jquery-1.11.0.min.js"></script>
+<script src="/scripts/pi.easings.js"></script>
+<script src="/scripts/pi.helpers.js"></script>
+<script src="/scripts/pi.boundManager.js"></script>
+<script src="/3dParty/inview.js"></script>
+<script src="/scripts/pi.imagesLoader.js"></script>
+<script src="/3dParty/gauge.min.js"></script>
+<script src="/scripts/pi.counter.js"></script>
+<script src="/scripts/pi.init.counter.js"></script>
+<script src="/scripts/pi.init.social.js"></script>
+<script src="/scripts/pi.ddMenu.js"></script>
+<script src="/scripts/pi.init.removeLastElMargin.js"></script>
+<script src="/scripts/pi.fixedHeader.js"></script>
+<script src="/scripts/pi.mobileMenu.js"></script>
+<script src="/scripts/pi.init.submitFormFooter.js"></script>
+<script src="/scripts/pi.detectTransition.js"></script>
+<script src="/scripts/pi.alert.js"></script>
+<script src="/scripts/pi.init.formsBlurClasses.js"></script>
+<script src="/scripts/pi.init.placeholder.js"></script>
+<script src="/3dParty/colorbox/jquery.colorbox-min.js"></script>
+<script src="/scripts/pi.init.colorbox.js"></script>
+<script src="/3dParty/jquery.easing.1.3.js"></script>
+<script src="/3dParty/jquery.scrollTo.min.js"></script>
+<script src="/scripts/pi.init.jqueryScrollTo.js"></script>
+<script src="/scripts/pi.scrollTopArrow.js"></script>
+<script src="/3dParty/tweetie/tweetie.min.js"></script>
+<script src="/scripts/pi.init.tweetie.js"></script>
+
+
+@stop
